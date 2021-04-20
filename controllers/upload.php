f8b0271e83e38023f4008278fad6b9eb4113ca4b
Snoet_ <tycho.brakenhoff@ictmbo.nl>
2021-04-20 14:26:17 +0200

    Added folders and changed a filetype requirement

--- controllers/upload.php
+++ controllers/upload.php
@@ -41,7 +41,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 }
 
 // Allow certain file formats
-if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "webp" && $imageFileType != "jpeg"
     && $imageFileType != "gif") {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
