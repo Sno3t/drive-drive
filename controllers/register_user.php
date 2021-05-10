3cfee20cb7505665bb265759a0d370dbdafd0e4d
LeviOS31 <levi.oldescheper@gmail.com>
2021-05-10 13:57:44 +0200

    aanpassingen aan register gemaakt

--- controllers/register_user.php
+++ controllers/register_user.php
@@ -1,6 +1,6 @@
 <?php
     // use strict typed programming
-
+    session_start();
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
         if(!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['password_repeat']) && !empty($_POST['email'])){
 
@@ -37,19 +37,22 @@
                     $pdo = new PDO($dsn, $user, $pass, $options);
                     // Insert the user into to database, using prepare()
 
-                    $stmt = $pdo->prepare("INSERT INTO `account`(`ID`, `Username`,`Hashpassword`,`email`,`bevoegtheid`) VALUES (NULL,?,?,?,1)");
-                    $stmt->execute([$username,$password_hash,$email]);
-                    if ($stmt->rowCount() === 1) {
-                        //header("redirect:5;url=?");
-                        echo "registratie is succesvol";
-                    } else if ($stmt->errorCode === '23000') {
-                        // Username already exists. Try another username.
-                        throw new PDOException("deze gebruikersnaam bestaat al <a href='../views/register.html'>terug</a>");
-                        // Create a link to the registration page.
-                    } else {
-                        // General error. Give a general message 
-                        throw new PDOException("er is iets misgegaan <a href='../views/register.html'>terug</a><br>" . $stmt->errorInfo() . " " . $stmt->errorCode());
-                        // Then give the errorInfo() and errorCode() in parenthesis.
+                    $stmt = $pdo->query("SELECT COUNT(*) FROM account WHERE Username = $username OR email = $email;");
+                    if($count = $stmt->fetchColumn() < 1){
+                        $stmt2 = $pdo->prepare("INSERT INTO `account`(`ID`, `Username`,`Hashpassword`,`email`,`bevoegtheid`) VALUES (NULL,?,?,?,1);");
+                        $stmt2->execute([$username,$password_hash,$email]);
+                        if ($stmt2->rowCount() === 1) {
+                            header("redirect:5;url=../views/overview.html");
+                            $_SESSION["username"] = $username;
+                            $_SESSION["loggedin"] = true;
+                            echo "registratie is succesvol";
+                        } else {
+                            // General error. Give a general message 
+                            throw new PDOException("er is iets misgegaan <a href='../views/register.html'>terug</a><br>" . $stmt->errorInfo() . " " . $stmt->errorCode());
+                            // Then give the errorInfo() and errorCode() in parenthesis.
+                        }
+                    } else{
+                        throw new PDOException("username or email is already in use <a href='../views/register.html'>terug</a>");
                     }
                 } catch (PDOException $e) {
                 // Give an error message:
